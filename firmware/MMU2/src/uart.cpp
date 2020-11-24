cb52b064a830883bc5aaa65b311d67b90b585178
BigTreeTech <38851044+bigtreetech@users.noreply.github.com>
2020-11-24 20:49:55 +0800

    Update uart.cpp

--- firmware/MMU2/src/uart.cpp
+++ firmware/MMU2/src/uart.cpp
@@ -36,13 +36,6 @@ int uart1_getchar(FILE *)
 	return Serial1.read();
 }
 
-
-void uart0_init(void)
-{
-	Serial.begin(UART0_BDR, SERIAL_8N1); //serial0 - USB
-	//fdev_setup_stream(uart0io, uart0_putchar, uart0_getchar, _FDEV_SETUP_WRITE | _FDEV_SETUP_READ); //setup uart in/out stream
-}
-
 void uart1_init(void)
 {
 	Serial1.begin(UART1_BDR, SERIAL_8N1); //serial1
